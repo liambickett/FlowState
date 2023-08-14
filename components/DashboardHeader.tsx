63c45bf6a8a108efa3853486947380dad99afd4f
liambickett <120602920+liambickett@users.noreply.github.com>
2023-08-14 02:36:31 -0700

    Style improvements

--- components/DashboardHeader.tsx
+++ components/DashboardHeader.tsx
@@ -3,20 +3,22 @@ import Clock from 'react-live-clock';
 export default function DashboardHeader() {
   return (
     <div
-      className='marker clock'
+      className=' clock-area'
       style={{ width: '100%', height: '50vh', paddingTop: 85 }}
     >
-      <Clock
-        style={{ fontSize: '3vh', fontWeight: 400 }}
-        className='dm'
-        format={'dddd, MMMM Mo, YYYY'}
-        ticking={true}
-      />
-      <Clock
-        style={{ fontSize: '10vh', fontWeight: 800 }}
-        format={'h:mm A'}
-        ticking={true}
-      />
+      <div className='clock'>
+        <Clock
+          style={{ fontSize: '3vh', fontWeight: 400 }}
+          className='dm'
+          format={'dddd, MMMM Mo, YYYY'}
+          ticking={true}
+        />
+        <Clock
+          style={{ fontSize: '10vh', fontWeight: 800, lineHeight: '10vh' }}
+          format={'h:mm A'}
+          ticking={true}
+        />
+      </div>
     </div>
   );
 }
